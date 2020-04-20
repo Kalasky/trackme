1fce0b9a73e0a0e3e9938eac35052df33256666d
Robert Kalasky <kalaskyr@gmail.com>
2020-04-20 00:07:31 -0700

    assign kd role automatic

--- commands/assignrole.js
+++ commands/assignrole.js
@@ -16,23 +16,6 @@ module.exports = {
                   `Discord ID: ${x.discordID}     battlenet ID: ${x.battlenetID}      KD: ${data.br.kdRatio}`
                 );
 
-                /**
-                 * PSEUDOCODE FOR ROLE IDENTIFICATION?
-                 *
-                 * if kills <= 400 -> PLEB
-                 * elseif kills > 400
-                 * comment: in this if, we put the whole condition of trying to find kd
-                 *    let kd;
-                 *    comment: round of kd to nearest two?
-                 *    if kd < 1 -> NOOBMASTER
-                 *    if kd >= 1 && kd <= 1.99 -> 1KD
-                 *    if kd >= 2 && kd <= 2.99 -> 2KD
-                 *    if kd >= 3 && kd <= 3.99 -> 3KD
-                 *    if kd >= 4 && kd <= 4.99 -> 4KD
-                 *    if kd >= 5 && kd <= 5.99 -> 5KD
-                 *    if kd >= 6 -> HACKER
-                 */
-
                 let obj = [
                   {
                     role_name: "PLEB",
@@ -102,48 +85,24 @@ module.exports = {
                 }
 
                 message.guild.members.fetch(x.discordID).then((member_data) => {
-                  // if (data.br.kdRatio > 1) {
-                  //   member_data.roles.add(getRole(object[x]['role_name'], message));
-                  // }
-
                   if (data.br.kills <= obj[0]["role_reqKills"]) {
-                    message.member_data.roles.add(
+                    member_data.roles.add(
                       getRole(obj[0]["role_name"], message)
                     );
                   } else {
                     for (let i = 1; i < obj.length; i++) {
                       if (
-                        data.br.kdRatio >= obj[1]["role_min_kd"] &&
-                        data.br.kdRatio <= obj[1]["role_max_kd"]
+                        data.br.kdRatio >= obj[i]["role_min_kd"] &&
+                        data.br.kdRatio <= obj[i]["role_max_kd"]
                       ) {
-                        message.member_data.roles.add(
-                          getRole(obj[1]["role_name"], message)
+                        member_data.roles.add(
+                          getRole(obj[i]["role_name"], message)
                         );
-                      } else if (
-                        data.br.kdRatio >= obj[2]["role_min_kd"] &&
-                        data.br.kdRatio <= obj[2]["role_max_kd"]
-                      ) {
-                        message.member_data.roles.add(
-                          getRole(obj[2]["role_name"], message)
-                        );
+                        break;
                       }
                     }
                   }
                 });
-
-                // assignRole = () => {
-                //   // start adding roles
-                //   let role = message.guild.roles.cache.find((x) => {
-                //     return x.name == obj[x].role_name;
-                //   });
-                // };
-
-                // message.guild.members.fetch(x.discordID).then((member_data) => {
-                //   if (data.br.kdRatio > 1) {
-                //     member_data.roles.add(role);
-                //   }
-                // });
-                // end
               })
               .catch((err) => {
                 console.log(err);
