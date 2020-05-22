bba0651641e5d24672e5e9dba0a3198ba896d603
Robert Kalasky <kalaskyr@gmail.com>
2020-05-21 21:25:07 -0700

    push

--- commands/ping.js
+++ commands/ping.js
@@ -1,26 +1,10 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-let CronJob = require("cron").CronJob;
-
-var job = new CronJob(
-  "* * * * * *",
-  function () {
-    client.on("message", (message) => {
-      console.log("You will see this message every second");
-      message.channel.send("Pong.");
-    });
-  },
-  null,
-  true,
-  "America/Los_Angeles"
-);
 
 module.exports = {
   name: "ping",
   cooldown: 5,
   description: "Ping!",
   execute(message, args) {
-    // message.channel.send("Pong.");
-    job.start();
+    message.channel.send("Pong.");
   },
 };
