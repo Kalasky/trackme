90b80f02c128bf0014567d82924b951ce1a5a05f
Robert Kalasky <kalaskyr@gmail.com>
2021-03-07 12:17:27 -0500

    trackuser command done

--- commands/args-info.js
+++ commands/args-info.js
@@ -1,21 +1,93 @@
 require("dotenv").config();
 const API = require("call-of-duty-api")();
+const Discord = require("discord.js");
 
 module.exports = {
-  name: "args-info",
-  syntax: "!args-info",
-  description: "Information about the arguments provided.",
+  name: "trackuser",
+  syntax: "!trackuser <username> <platform>",
+  description:
+    "If a user isn't being tracked under the bot you can track them yourself!",
   include: true,
   args: false,
   execute(message, args) {
-    API.login(process.env.COD_EMAIL, process.env.COD_PASSWORD).then((data) => {
-      API.CWmp("kal#2498559", "acti").then((data) => {
-        console.log(data.lifetime.itemData.weapon_smg);
-      });
-    });
+    let pattern = /".*?"/g;
+    let output = pattern.exec(message.content);
+    let id;
+    let count;
+    let platform;
 
-    message.channel.send(
-      `Arguments: ${args}\nArguments length: ${args.length}`
-    );
+    if (output == null) {
+      id = args[0];
+      platform = args[1];
+    } else {
+      id = output[0].slice(1, -1);
+      console.log(id);
+      count = output[0].split(" ").length;
+      platform = args[count];
+    }
+
+    console.log(message.content);
+
+    API.login(process.env.COD_EMAIL, process.env.COD_PASSWORD).then((data) => {
+      API.MWBattleData(id, platform)
+        .then((warzoneData) => {
+          let kd = warzoneData.br.kdRatio.toFixed(6).slice(0, -4);
+          let kills = warzoneData.br.kills;
+          let wins = warzoneData.br.wins;
+          let topTen = warzoneData.br.topTen;
+          let topFive = warzoneData.br.topFive;
+          // console.log(kd, kills, wins);
+          // console.log(data.lifetime.itemData.weapon_smg);
+
+          const trackedUserEmbed = new Discord.MessageEmbed()
+            .setColor("#4BB543")
+            .setTitle(`${id}'s Warzone Stats`)
+            // .setDescription(
+            //   "Enter in your gamertag with the corresponding platform. If you enter invalid credentials you have a maximum of three tries before the bot exits the prompt."
+            // )
+            .addFields(
+              {
+                name: "KDR",
+                value: kd,
+                inline: true,
+              },
+
+              {
+                name: "Kills",
+                value: kills,
+                inline: true,
+              },
+              {
+                name: "Wins",
+                value: wins,
+                inline: true,
+              },
+              {
+                name: "Top 10",
+                value: topTen,
+                inline: true,
+              },
+              {
+                name: "Top 5",
+                value: topFive,
+                inline: true,
+              }
+            )
+            .setThumbnail("https://i.imgur.com/pfXAuiY.png");
+
+          message.channel.send(trackedUserEmbed);
+        })
+        .catch((err) => {
+          const trackUserError = new Discord.MessageEmbed()
+            .setColor("#FF0000")
+            .setTitle("Error")
+            .setDescription(
+              '- Syntax: `!trackuser <username> <platform>` \n- If the username contains spaces wrap it in quotes. \n- Example for a name with spaces: `!trackuser "TT Cudi" xbl` \n- Make sure the username and platform are corresponding.  \n- You may also have provided an incorrect username or platform. '
+            )
+            .setThumbnail("https://i.imgur.com/I6hxLXI.png");
+
+          message.channel.send(trackUserError);
+        });
+    });
   },
 };
